dc8e81e72efac99faecc410394af97a936543ba5
Alisa Umnova <alisa.umnova@gmail.com>
2018-05-23 15:11:59 +0300

    corrected

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,23 +5,23 @@ app.controller('myCtrl', function ($scope, $http) {
 	$scope.submit = function () {
 		console.log('clicked submit');
 		$http({
-				url: '/user'
-				, method: 'POST'
-				, data: $scope.data
-			}).then(function (httpResponse) {
-				$scope.tweetArr = [];
-				var obj = {};
-				console.log(httpResponse.data);
-				for (var i = 0; i < httpResponse.data.length; i++) {
-					obj.text = httpResponse.data[i].text;
-					obj.name = httpResponse.data[i].user.name;
-					obj.created_at = httpResponse.data[i].created_at;
-					$scope.tweetArr.push(obj);
-					obj = {};
-				}
-				//console.log('response:', $scope.tweetArr);
-			}).catch(function (response) {
-				console.error(response);
-			})
+			url: '/user'
+			, method: 'POST'
+			, data: $scope.data
+		}).then(function (httpResponse) {
+			$scope.tweetArr = [];
+			var obj = {};
+			console.log(httpResponse.data);
+			for (var i = 0; i < httpResponse.data.length; i++) {
+				obj.text = httpResponse.data[i].text;
+				obj.name = httpResponse.data[i].user.name;
+				obj.created_at = httpResponse.data[i].created_at;
+				$scope.tweetArr.push(obj);
+				obj = {};
+			}
+			//console.log('response:', $scope.tweetArr);
+		}).catch(function (response) {
+			console.error(response);
+		})
 	}
 });
